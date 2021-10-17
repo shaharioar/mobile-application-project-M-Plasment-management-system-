28f1c733653142056485b347fd4474179d0a9bec
shaharioar <45413876+shaharioar@users.noreply.github.com>
2021-10-17 23:31:01 +0530

    first commit

---
+++ lib/widgets/parent/fees_tile.dart
@@ -0,0 +1,35 @@
+/* the ListTile that we will show every fee information in */
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class FeesTile extends StatelessWidget {
+  final String amount;
+  final String date;
+
+  FeesTile(
+      {this.amount="", this.date="", });
+
+  @override
+  Widget build(BuildContext context) {
+   
+
+    return Column(
+      children: <Widget>[
+       
+        
+               ListTile(
+
+        
+           
+                title: Text(amount,style: GoogleFonts.antic(fontWeight:FontWeight.bold,fontSize: 20),),
+            subtitle: Text(date),
+          ),
+      
+        
+        Divider(thickness: 1.5,)
+      ],
+    );
+  }
+}
